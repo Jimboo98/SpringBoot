009bf5b2db5584ae609cb4bcfe3d00d90338eebf
Jimbo <orozco.jaime25@gmail.com>
2023-05-26 18:58:01 -0600

    ....

---
+++ spring-boot-web-di/spring-boot-di/src/main/java/com/jimbo/springboot/app/springbootdi/Models/service/MiServicio.java
@@ -0,0 +1,8 @@
+package com.jimbo.springboot.app.springbootdi.Models.service;
+
+public class MiServicio {
+    
+    public String operacion(){
+        return "ejecutando algun proceso importante...";
+    }
+}
--- spring-boot-web-di/spring-boot-di/src/main/java/com/jimbo/springboot/app/springbootdi/controllers/IndexController.java
+++ spring-boot-web-di/spring-boot-di/src/main/java/com/jimbo/springboot/app/springbootdi/controllers/IndexController.java
@@ -1,12 +1,20 @@
 package com.jimbo.springboot.app.springbootdi.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jimbo.springboot.app.springbootdi.Models.service.MiServicio;
 
 @Controller
 public class IndexController{
 
-    
+    private MiServicio servicio = new MiServicio();
+
+    @GetMapping({"/index"," ", "/"})
     public String index(Model model){
+        model.addAttribute("objecto", servicio.operacion());
         return "index";
     }
 
