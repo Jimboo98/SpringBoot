37d490ff4eb10cffe6190c61602dd72e6965e56d
Jimbo <orozco.jaime25@gmail.com>
2023-05-26 18:10:31 -0600

    Creation index controller file

---
+++ spring-boot-web-di/spring-boot-di/src/main/java/com/jimbo/springboot/app/springbootdi/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.jimbo.springboot.app.springbootdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController{
+
+    public String index(Model model){
+        return "index";
+    }
+
+}
+
